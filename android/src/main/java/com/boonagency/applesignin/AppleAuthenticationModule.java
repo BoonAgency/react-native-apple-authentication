d1b78995d7e096402e00694c1ca5967ad849cded
Jose Bravo <ja.bravo.isidro@gmail.com>
2020-01-20 12:05:45 +0100

    Change from reactlibrary to boonagency.applesignin

--- android/src/main/java/com/boonagency/applesignin/AppleAuthenticationModule.java
+++ android/src/main/java/com/boonagency/applesignin/AppleAuthenticationModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.boonagency.applesignin;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
